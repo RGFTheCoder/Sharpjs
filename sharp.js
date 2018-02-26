fbc9925ea8aea31105b27e989caa4686005f009c
RGFTheCoder <recdeclec.ceo@gmail.com>
2018-02-25 19:26:17 -0500

    Update sharp.js

--- sharp.js
+++ sharp.js
@@ -30,9 +30,27 @@ var Sharp = {
     })
 }
 
-window.onload = function() {
+loadSharp = function() {
     Sharp.start();
     setInterval(Sharp.run, 10);
 }
 
-// hola soy papa
+function addLoadEvent(func) {
+  var oldonload = window.onload;
+  if (typeof window.onload != 'function') {
+    window.onload = func;
+  } else {
+    window.onload = function() {
+      if (oldonload) {
+        oldonload();
+      }
+      func();
+    }
+  }
+}
+addLoadEvent(nameOfSomeFunctionToRunOnPageLoad);
+addLoadEvent(function() {
+  /* more code to run on page load */
+});
+
+addLoadEvent(loadSharp);
